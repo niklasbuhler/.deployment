1d0e213860696621bcdfcccc7c30cda8a5ea82b5
Niklas Bühler <62268629+niklasbuhler@users.noreply.github.com>
2022-01-19 21:29:47 +0100

    various fixes

--- openbsd.sh
+++ openbsd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-during setup:
+####################### setup
 
-Install ugprade auztinstall shell? -> s
+# Install ugprade auztinstall shell? -> s
 kbd de
 # show disks
 sysctl hw.disknames
@@ -14,49 +14,58 @@ sysctl hw.disknames
 
 ########################### ROOT
 fw_update -a 
-pkg_add emacs vim lilypond git git-crypt github-cli groff rsync stow curl unison unison ledger rclone ffmpeg sox borgbackup password-store flac
-echo 'permit persist niklas' > /etc/doas.conf
+echo 'permit persist keepenv niklas' > /etc/doas.conf
 
 ################################################ USER
 # install desktop
-doas pkg_add dunst picom feh zathura zathura-pdf-poppler zathura-ps syncthing neofetch youtube-dl mumble audacity blender gimp mpv  thunderbird fish mpd
+doas pkg_add emacs vim lilypond git git-crypt github-cli groff rsync stow curl unison ledger rclone ffmpeg sox borgbackup password-store flac
+doas pkg_add dunst picom feh zathura zathura-pdf-poppler zathura-ps syncthing neofetch youtube-dl mumble audacity blender gimp mpv thunderbird fish mpd
 # must maybe add /usr/local/bin/fish to /etc/shells
 chsh -s /usr/local/bin/fish
 doas rcctl enable xenodm
 doas rcctl enable mpd
+doas rcctl enable apmd
+doas rcctl set apmd flags -A
+doas rcctl start apmd
+# clean up
+doas sed -i 's/xconsole/#xconsole/' /etc/X11/xenodm/Xsetup_0
+doas echo 'xset b off' >> /etc/X11/xenodm/Xsetup_0
+# dont ping google
+doas sed -i 's/www\.google\.com/www\.openbsd\.org/' /etc/ntpd.conf
 
+# configure resource limits
+doas usermod -G staff niklas
 
 gh auth login
 git config --global user.name "Niklas Bühler"
 git config --global user.email "niklas@klangstellwerk.de"
 git config --global init.defaultBranch master
-mkdir ~/git
 
 # create dirs
 mkdir ~/git
 # do it
 cd ~/git
-git clone https://git.suckless.org/dwm
-cd dwm/
+git clone https://github.com/niklasbuhler/dwm-openbsd.git
+cd dwm-openbsd/
+doas make clean install
+
+cd ~/git
+git clone https://github.com/niklasbuhler/dmenu-openbsd.git
+cd dmenu-openbsd/
 make clean install
 
-cd /root/git
-git clone https://git.suckless.org/dmenu
-cd dmenu/
+cd ~/git
+git clone https://github.com/niklasbuhler/slstatus-openbsd.git
+cd slstatus-openbsd/
 make clean install
 
-cd /root/git
-git clone https://git.suckless.org/slstatus
-cd slstatus/
-make clean install
-
-cd /root/git
-git clone https://git.suckless.org/tabbed
+cd ~/git
+git clone https://github.com/niklasbuhler/tabbed-openbsd.git
 cd tabbed/
 make clean install
 
-cd /root/git
-git clone https://git.suckless.org/st
+cd ~/git
+git clone https://github.com/niklasbuhler/st-openbsd.git
 cd st/
 make clean install
 
@@ -66,11 +75,12 @@ git clone https://github.com/niklasbuhler/.dotfiles.git ~/git/dotfiles
 
 mkdir ~/.emacs.d
 stow ~/git/dotfiles/emacs -t ~/
-stow ~/git/dotfiles/xorg/ -t ~/
+cd ~/git/dotfiles && stow xorg/ -t ~/
+cd ~/
 mkdir ~/.config
 mkdir ~/.config/fish
 rm ~/.config/fish/config.fish
-stow ~/git/dotfiles/fish -t ~/
+cd ~/git/dotfiles && stow fish/ -t ~/
 
 doas ifconfig iwm0 up
 doas ifconfig iwm0 nwid "IO" wpakey "XXXXXXXXXXXXXX"
